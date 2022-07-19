7497a135d7b436589ee6e87c6ac52e3c28f9fe74
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-07-19 13:36:56 +0100

    USER_LOGOUT, ADMIN_LOGOUT, ADMIN_LOGIN & ADMIN_CREATED event handler

--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 import {
+  ADMIN_CREATED,
+  ADMIN_CREATED_SESSION_REGISTERED,
+  ADMIN_LOGIN,
+  ADMIN_LOGOUT,
   USER_CREATED,
   USER_CREATED_SESSION_REGISTERED,
   USER_LOGIN,
+  USER_LOGOUT,
 } from 'src/app.events';
 import { User, UserDocument } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class SessionService {
   constructor(private eventEmitter: EventEmitter2) {}
+
   @OnEvent(USER_CREATED)
   handleUserCreatedEvent(
     session: Record<string, any>,
@@ -19,6 +25,31 @@ export class SessionService {
     session[key] = data;
     this.eventEmitter.emit(USER_CREATED_SESSION_REGISTERED);
   }
+
+  @OnEvent(ADMIN_CREATED)
+  handleAdminCreatedEvent(
+    session: Record<string, any>,
+    key: string,
+    data: User,
+  ) {
+    session[key] = data;
+    this.eventEmitter.emit(ADMIN_CREATED_SESSION_REGISTERED);
+  }
+
+  @OnEvent(ADMIN_LOGIN)
+  handleAdminLoginEvent(
+    session: Record<string, any>,
+    key: string,
+    data: UserDocument,
+  ) {
+    session[key] = data;
+  }
+
+  @OnEvent(ADMIN_LOGOUT)
+  handleAdminLogoutEvent(session: Record<string, any>, key: string) {
+    session[key] = null;
+  }
+
   @OnEvent(USER_LOGIN)
   handleUserLoginEvent(
     session: Record<string, any>,
@@ -26,6 +57,10 @@ export class SessionService {
     data: UserDocument,
   ) {
     session[key] = data;
-    this.eventEmitter.emit(USER_CREATED_SESSION_REGISTERED);
+  }
+
+  @OnEvent(USER_LOGOUT)
+  handleUserLogoutEvent(session: Record<string, any>, key: string) {
+    session[key] = null;
   }
 }

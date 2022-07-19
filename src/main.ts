f1d17b15b7cb548b1ab3d7cbca940a5033dd49be
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-07-19 16:37:43 +0100

    change secret

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as session from 'express-session';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     session({
-      secret: 'my-secret',
+      secret: '$2b$15$Bzobfwji9AwUE7tWNxw8fuWVqUVWTDRuV8OBA4kwO3gorgaQ5nH.2',
       resave: false,
       saveUninitialized: false,
     }),
   );
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const config = new DocumentBuilder()
     .setTitle('Nest store api')
     .setDescription('This is an api for a nest store')

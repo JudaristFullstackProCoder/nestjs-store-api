3676f88b36bc76d41f34b51ef4e4b94442e00f32
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-07-19 13:34:36 +0100

    add endpoint to se user session content

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Session } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('session')
+  getSession(@Session() session: Record<string, unknown>) {
+    return session;
   }
 }

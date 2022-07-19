3d2586b476c7d6c8f8a2a196a5c0b3e720606a14
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-07-19 23:37:03 +0100

    update create user dto, crate add & remove user perms dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
   username: string;
   @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
   password: string;
   @ApiProperty()
-  @IsEmail()
   email: string;
 }
